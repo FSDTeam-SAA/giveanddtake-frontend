b2e68bf3d83f4444712dfeb414e39c97778179e3
sozibhossain <sozibhossain69@gmail.com>
2025-07-23 14:39:18 +0600

    added

--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -1,10 +1,9 @@
-import { SiteHeader } from "@/components/shared/site-header";
+
 import { HeroSection } from "@/components/hero-section";
 import { HowItWorksSection } from "@/components/how-it-works-section";
 import { SectorSection } from "@/components/sector-section";
 import { RecentJobsSection } from "@/components/recent-jobs-section";
 import { PricingSection } from "@/components/pricing-section";
-import { Footer } from "@/components/shared/footer"; // Import the new Footer component
 
 export default function Home() {
   return (
