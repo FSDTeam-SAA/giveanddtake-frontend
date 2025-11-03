4e779f728ecc2093dbacc0c7e4cd3b06133798e4
Fahim <ahmed.fahim37@gmail.com>
2025-11-03 15:42:42 +0600

    updates

--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -1,3 +1,4 @@
+import ChatbotWidget from "@/components/chatbot-widget";
 import { HeroSection } from "@/components/hero-section";
 import { HowItWorksSection } from "@/components/how-it-works-section";
 import { Suspense } from "react";
@@ -14,6 +15,7 @@ export default function Home() {
         {/* <RecentJobsSection /> */}
         {/* <PricingSection /> */}
         {/* <CandidateRecruiterCompanyList /> */}
+         <ChatbotWidget />
       </Suspense>
     </div>
   );
